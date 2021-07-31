54962331db99387b22db585ab253948adcf5c39d
mahdi-gnb <qanbarim327@hotmail.com>
2021-07-31 15:54:35 +0430

    Only Authorized and valid User can see his/her task

--- ToDoList/Controllers/ToDoItemsController.cs
+++ ToDoList/Controllers/ToDoItemsController.cs
@@ -14,6 +14,12 @@ using ToDoList.Data;
 using Serilog;
 using Google.Apis.Drive.v3;
 using System;
+using System.Net.Http;
+using System.Net;
+using ToDoList.Services;
+using Microsoft.AspNet.Identity;
+using System.Security.Claims;
+using System.ServiceModel.Channels;
 
 namespace ToDoList.Controllers
 {
@@ -26,56 +32,60 @@ namespace ToDoList.Controllers
         public ToDoItemsController(DatabaseContext context)
         {
             _context = context;
+
         }
 
         #region ShowTasks(admin)
         // GET: api/<ToDoItemsController>
-        [Authorize(Roles = "Administrator")]
+        [Authorize(Roles ="User,Admin")]
         [SwaggerOperation(Tags = new[] { "ShowTasks" })]
         [HttpGet(Name = "ShowTasks")]
         public async Task<ActionResult<IEnumerable<ToDoItems>>> Get()
         {
-            return await _context.toDoItems.ToListAsync();
-            //if (username == Data.User.UserName && password==Data.User.Password)
-            //{
-            //return await _context.toDoItems.ToListAsync();
-            //}
-            //else
-            //{
-            //    return NotFound();
-            //}
+            return await _context.toDoItems
+                .Where(option => option.User.UserName == User.Identity.Name)
+                .ToListAsync();
         }
         #endregion
 
-        #region ShowTask(User)
+        #region ShowTaskById(User)
         // GET api/<ToDoItemsController>/5
-        [Authorize]
+        [Authorize()]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [SwaggerOperation(Tags = new[] { "ShowTasks" })]
         [HttpGet("{id}")]
         public async Task<ActionResult<ToDoItems>> Get(long id)
         {
-            var todoItem = await _context.toDoItems.FindAsync(id);
+            var todo = _context.toDoItems;
+            bool currentUserTask = todo.AsQueryable().Any(e => e.Id == id && e.User.UserName == User.Identity.Name);
 
-            if (todoItem == null)
+            if (currentUserTask ==false)
             {
                 return NotFound();
             }
-
-            return todoItem;
+            var todoItem = await _context.toDoItems.FindAsync(id);
+            return Ok(todoItem);
         }
         #endregion
 
         #region CreateTasks(User)
         // POST api/<ToDoItemsController>
-        [Authorize(Roles = "User")]
+        [Authorize(Roles ="User")]
         [SwaggerOperation(Tags = new[] { "CreateTasks" })]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ActionName(nameof(ToDoItems))]
         [HttpPost]
         public async Task<ActionResult<ToDoItems>> CreateTodoItem(ToDoItems toDo)
         {
+            //toDo.UserId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            toDo.Author = User.Identity.Name;
+            toDo.UserId = _context.User
+                .Where(m => m.UserName == toDo.Author)
+                .Select(m => m.Id)
+                .SingleOrDefault();
+            //var y = User.Identity.GetUserName();
+            //var userResult = _context.User.FirstOrDefault(user => user.Id == toDo.UserId);
             _context.toDoItems.Add(toDo);
             await _context.SaveChangesAsync();
 
@@ -88,14 +98,18 @@ namespace ToDoList.Controllers
         [Authorize(Roles = "User")]
         [SwaggerOperation(Tags = new[] { "UpdateTasks" })]
         [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateTodoItem(long id, ToDoItems todoItems)
+        public async Task<ActionResult<string>> UpdateTodoItem(long id, ToDoItems todoItems)
         {
-            if (id != todoItems.Id)
+            if (id != todoItems.Id && todoItems.Author != User.Identity.Name)
             {
                 return BadRequest();
             }
             _context.Entry(todoItems).State = EntityState.Modified;
-
+            todoItems.Author = User.Identity.Name;
+            todoItems.UserId = _context.User
+                .Where(m => m.UserName == todoItems.Author)
+                .Select(m => m.Id)
+                .SingleOrDefault();
             try
             {
                 await _context.SaveChangesAsync();
@@ -112,7 +126,7 @@ namespace ToDoList.Controllers
                 }
             }
 
-            return NoContent();
+            return "Value changed Successfully";
         }
         #endregion
 
@@ -120,44 +134,45 @@ namespace ToDoList.Controllers
         // DELETE api/<ToDoItemsController>/5
         [HttpDelete("{id}")]
         [SwaggerOperation(Tags = new[] { "DeleteTasks" })]
-        public async Task<IActionResult> DeleteToDoItem(long id)
+        public async Task<ActionResult<string>> DeleteToDoItem(long id)
         {
-            var todoItem = await _context.toDoItems.FindAsync(id);
+            var todo = _context.toDoItems;
+            bool currentUserTask = todo.AsQueryable().Any(e => e.Id == id && e.User.UserName == User.Identity.Name);
 
-            if (todoItem == null)
+            if (currentUserTask == false)
             {
                 return NotFound();
             }
-
+            var todoItem = await _context.toDoItems.FindAsync(id);
             _context.toDoItems.Remove(todoItem);
             await _context.SaveChangesAsync();
-
-            return NoContent();
+            return $"Task {id} deleted succesfully";
         }
         #endregion
 
 
-        [ActionName(nameof(ToDoItems))]
-        [SwaggerOperation(Tags = new[] { "CreateTasks" })]
-        [Route("test/{id}")]
-        [HttpPut]
-        public async Task<IActionResult> CreateTodoItem(long id, ToDoItems todo)
-        {
-            if (id != todo.Id)
-            {
-                return BadRequest();
-            }
-            _context.Entry(todo).State = EntityState.Modified;
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (Exception)
-            {
+        //[ActionName(nameof(ToDoItems))]
+        //[SwaggerOperation(Tags = new[] { "CreateTasks" })]
+        //[Route("{id}")]
+        //[HttpPut]
+        //public async Task<ActionResult<string>> CreateTodoItem(long id, ToDoItems todo)
+        //{
+        //    if (id != todo.Id)
+        //    {
+        //        return BadRequest();
+        //    }
+        //    _context.Entry(todo).State = EntityState.Modified;
+        //    try
+        //    {
+        //        await _context.SaveChangesAsync();
+        //    }
+        //    catch (Exception)
+        //    {
 
-            }
-            return Accepted();
-        }
+        //    }
+        //    //return Accepted();
+        //    return "Value changed successfully";
+        //}
         private bool TodoItemExists(long id) =>
              _context.toDoItems.Any(e => e.Id == id);
     }
